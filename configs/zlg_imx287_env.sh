53991e418eba7c96971ec59427374f5161620e6b
Phil Han <pengphei@sina.com>
2017-05-17 23:27:28 +0800

    Update building script

--- configs/zlg_imx287_env.sh
+++ configs/zlg_imx287_env.sh
@@ -15,6 +15,7 @@ export BUILD_ARCH=arm
 export BUILD_CROSS_COMPILE=arm-none-linux-gnueabi-
 export BUILD_UBOOT_CONFIG=mx28_evk_config
 export BUILD_KERNEL_CONFIG=imx28_zlg_defconfig
+export BINARY_KERNEL=zImage
 
 export PATH=$PATH:${BUILD_TOOLCHAIN_PATH}:${BUILD_UBOOT_PATH}/tools:${BUILD_TRUNK}/package/bootloader/elftosb
 export LANG=C
@@ -31,80 +32,72 @@ function build_prepare()
 # build uboot
 function build_uboot()
 {
-    cd ${BUILD_UBOOT_PATH}
-    make ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} distclean
-    make ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} ${BUILD_UBOOT_CONFIG}
-    make ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE}
-    cp ${BUILD_UBOOT_PATH}/u-boot.bin ${BUILD_TRUNK_OUT}
-    cp ${BUILD_UBOOT_PATH}/u-boot ${BUILD_BOOTLETS_PATH}
-    cd -
+    make -C ${BUILD_UBOOT_PATH} ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/uboot distclean
+
+    make -C ${BUILD_UBOOT_PATH} ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/uboot ${BUILD_UBOOT_CONFIG}
+
+    make -C ${BUILD_UBOOT_PATH} ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/uboot 
+
+    cp ${BUILD_TRUNK_OUT}/uboot/u-boot.bin ${BUILD_TRUNK_OUT}
+    cp ${BUILD_TRUNK_OUT}/uboot/u-boot ${BUILD_TRUNK_OUT}
 }
 
 # build bootlets
 function build_bootlets()
 {
-    cd ${BUILD_BOOTLETS_PATH}
-
     if [ "i386" = ${HOSTTYPE} ];then
         export BUILD_ELFTOSB=elftosb_32bit
     else
         export BUILD_ELFTOSB=elftosb_64bit
     fi
 
+    mkdir ${BUILD_TRUNK_OUT}/bootlets
+
     # copy uboot to bootlets directory
-    cp ${BUILD_UBOOT_PATH}/u-boot ${BUILD_BOOTLETS_PATH}
+    cp ${BUILD_TRUNK_OUT}/u-boot ${BUILD_BOOTLETS_PATH}
     # copy kernel zImage to bootlets directory
-    cp ${BUILD_LINUX_PATH}/arch/${BUILD_ARCH}/boot/zImage ${BUILD_BOOTLETS_PATH}
+    cp ${BUILD_TRUNK_OUT}/zImage ${BUILD_BOOTLETS_PATH}
 
     # build bootlets image
-    make CROSS_COMPILE=${BUILD_CROSS_COMPILE} ELFTOSB=${BUILD_ELFTOSB} BOARD=iMX28_EVK
+    make -C ${BUILD_BOOTLETS_PATH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} ELFTOSB=${BUILD_ELFTOSB} BOARD=iMX28_EVK
     
     # copy uboot final image to output
 
     cp ${BUILD_BOOTLETS_PATH}/*.sb ${BUILD_TRUNK_OUT}
-
-    cd -
 }
 
 # build kernel
 function build_kernel()
-{
-    cd ${BUILD_LINUX_PATH}
+{   
+    mkdir -p ${BUILD_TRUNK_OUT}/linux
 
-    if [ -d output ]; then
-        rm -rf output
-        mkdir output
-    else
-        mkdir output
-    fi
-    
     # check if we have config used
-    if [ ! -e .config ]; then
-        cp arch/${BUILD_ARCH}/configs/${BUILD_KERNEL_CONFIG} .config
-    fi
+    make -C ${BUILD_LINUX_PATH} ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/linux ${BUILD_KERNEL_CONFIG}
 
     # build kernel uImage and modules
-    make ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} uImage modules
+    make -C ${BUILD_LINUX_PATH} ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/linux ${BINARY_KERNEL}
+
+    cp ${BUILD_TRUNK_OUT}/linux/arch/${BUILD_ARCH}/boot/${BINARY_KERNEL} ${BUILD_TRUNK_OUT}
+}	
+
+# build kernel modules
+function build_modules()
+{
+    rm -rf ${BUILD_TRUNK_OUT}/modules
+
+    make -C ${BUILD_LINUX_PATH} ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/linux modules
 
     # install modules to target directory
-    make INSTALL_MOD_PATH=output ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} modules_install
+    make -C ${BUILD_LINUX_PATH} INSTALL_MOD_PATH=${BUILD_TRUNK_OUT}/modules ARCH=${BUILD_ARCH} CROSS_COMPILE=${BUILD_CROSS_COMPILE} O=${BUILD_TRUNK_OUT}/linux modules_install
 
     # get kernel version
-    if [ -r include/generated/utsrelease.h ]; then
-        KERNEL_VERSION=`cat include/generated/utsrelease.h |awk -F\" '{print $2}'`
+    if [ -r ${BUILD_TRUNK_OUT}/linux/include/generated/utsrelease.h ]; then
+        KERNEL_VERSION=`cat ${BUILD_TRUNK_OUT}/linux/include/generated/utsrelease.h |awk -F\" '{print $2}'`
+        echo "kernel version ${KERNEL_VERSION}"
     fi
 
-    BUILD_KERNEL_MODULES_OUT=${BUILD_LINUX_PATH}/output/lib/modules
-
-    cp ${BUILD_LINUX_PATH}/arch/${BUILD_ARCH}/boot/uImage ${BUILD_TRUNK_OUT}
-    cp ${BUILD_LINUX_PATH}/arch/${BUILD_ARCH}/boot/zImage ${BUILD_TRUNK_OUT}
-
-    rm -rf ${BUILD_TRUNK_OUT}/modules
-    cp -r ${BUILD_KERNEL_MODULES_OUT} ${BUILD_TRUNK_OUT}
-    rm -rf ${BUILD_TRUNK_OUT}/modules/${KERNEL_VERSION}/build
-    rm -rf ${BUILD_TRUNK_OUT}/modules/${KERNEL_VERSION}/source
-
-    cd -
-}	
+    rm -rf ${BUILD_TRUNK_OUT}/modules/lib/modules/${KERNEL_VERSION}/build
+    rm -rf ${BUILD_TRUNK_OUT}/modules/lib/modules/${KERNEL_VERSION}/source
+}
 
 build_prepare
